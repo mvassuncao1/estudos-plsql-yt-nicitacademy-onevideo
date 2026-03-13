67b7ccd8ce76025d7b330e9c63e426e23f89b05f
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-13 14:09:27 -0300

    ESTUDOS...

---
+++ add_two_numbers.sql
@@ -0,0 +1,16 @@
+// Sempre finalizar com / após END
+BEGIN
+DBMS_OUTPUT.PUT_LINE ('TESTEacasc');
+END;
+/
+
+declare
+n1 number(10);
+n2 number(10);
+
+
+begin
+dbms_output.put_line('10 + 20');
+
+end;
+/
