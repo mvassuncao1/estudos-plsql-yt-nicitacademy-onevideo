bfedf9fc55915e3337d7e23ffba6adc1699e73d3
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-13 10:03:32 -0300

    Iniciei a aula

---
+++ inicio.sql
@@ -0,0 +1,17 @@
+/*
+Two blocks in  PLSQL
+
+1. Anonymous Block */
+
+DECLARE 
+-- Variable declarations
+    
+    
+
+/*
+2. Named Block - Stored as an object in DB
+    - PROCEDURE
+    - FUNCTION
+    - PACKAGE
+    - TRIGGER
+*/
