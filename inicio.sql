e6602c67853e2108a9fd1102491da63e02dc8085
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-13 10:36:15 -0300

    Update inicio.sql

--- inicio.sql
+++ inicio.sql
@@ -4,9 +4,16 @@ Two blocks in  PLSQL
 1. Anonymous Block */
 
 DECLARE 
--- Variable declarations
+    -- Variable declarations / opctional
     
+BEGIN
+    -- Exatutable statements / mandatory
     
+    EXCEPTION
+        -- Exception handling code / optional
+
+END;
+/
 
 /*
 2. Named Block - Stored as an object in DB
