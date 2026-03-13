d25167becd997622f8f999131cae355dad32ab86
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-13 16:00:02 -0300

    Estudei adicao de inteiros e decimais

--- add_two_numbers.sql
+++ add_two_numbers.sql
@@ -1,17 +1,47 @@
-// Sempre finalizar com / após END
+-- Sempre finalizar com / após END
 BEGIN
 DBMS_OUTPUT.PUT_LINE('TEasdasdsaSTE');
 END;
 /
 
 DECLARE
-n1 NUMBER(10);
-n2 NUMBER(10);
+-- Declaração de variáveis
+-- Podemos atribuir valores na declaração
+n1 NUMBER(10) := 10;
+n2 NUMBER(10) := 20;
 
 BEGIN
+-- Podemos alterar valores das variáveis no BEGIN
 n1 := 10;
 n2 := 6;
-DBMS_OUTPUT.put_line('A soma é ' || (n1 + n2));
+dbms_output.put_line('A soma de ' || n1 || ' + ' || n2 || ' = ' || (n1 + n2));
+END;
+/
 
+-- Valores decimais em variáveis inteiras:
+DECLARE
+-- Declaração de variáveis
+-- Podemos atribuir valores na declaração
+n1 NUMBER(10) := 10.5;
+n2 NUMBER(10) := 20.5;
+
+BEGIN
+-- Podemos alterar valores das variáveis no bloco BEGIN
+n1 := 10.49; -- arredonda para baixo = 10
+n2 := 6.5;   -- arredonda para cima  = 7
+dbms_output.put_line('A soma de ' || n1 || ' + ' || n2 || ' = ' || (n1 + n2));
+END;
+/
+
+-- Para valores decimais serem exibidos corretamente mudamos a declaração
+DECLARE
+-- Declaração de variáveis
+-- Podemos atribuir valores na declaração
+n1     NUMBER(10, 2) := 10.5;
+n2     NUMBER(10, 2) := 20.5;
+result NUMBER(10, 2);
+BEGIN
+result := n1 + n2;
+dbms_output.put_line('A soma de ' || n1 || ' + ' || n2 || ' = ' || result);
 END;
 /
