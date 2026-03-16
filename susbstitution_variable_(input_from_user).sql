832ebbcb55e4176b7ccd765c2e77e55972485c53
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-16 08:45:57 -0300

    1:00:25 - estudei constantes, valores null

--- susbstitution_variable_(input_from_user).sql
+++ susbstitution_variable_(input_from_user).sql
@@ -7,3 +7,17 @@ result := n1 + n2;
 dbms_output.put_line('A soma de ' || n1 || ' + ' || n2 || ' = ' || result);
 END;
 /
+
+SET SERVEROUTPUT ON;
+DECLARE
+-- n3 number(10, 2) NOT NULL; -- dá erro pois n3 não pode receber valores nulos.
+n3 number(10, 2);
+n4 number(10, 2);
+r1 number(10, 2);
+BEGIN
+n3 := null;
+n4 := 10;
+r1 := n3 + n4;
+dbms_output.put_line('A soma de ' || n3 || ' + ' || n4 || ' = ' || r1); -- apresenta valores em branco de n3 e r1 pois n3 tem valor NULL
+END;
+/
