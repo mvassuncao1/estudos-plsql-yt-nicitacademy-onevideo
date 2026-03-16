7ffd1a1890577de466bf84ba0c4066083cadc7f8
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-16 13:52:28 -0300

    CREATE_TABLE e INSERT_INTO

--- INSERT_statement.sql
+++ INSERT_statement.sql
@@ -1,3 +1,6 @@
+-- Pode executar instrução direta
+INSERT INTO funcionario (nome, salario) VALUES ('Gustavo', 653.45);
+
 DECLARE
 nome_func           varchar2(100);
 salario_inicial     number(10, 2);
@@ -5,5 +8,15 @@ BEGIN
 nome_func           := 'João Vitor 1598874';
 salario_inicial     := 1791.05;
 INSERT INTO FUNCIONARIO (NOME, SALARIO) VALUES (nome_func, salario_inicial);
+COMMIT;
+END;
+/
+
+BEGIN
+INSERT INTO FUNCIONARIO
+            (NOME,          SALARIO) VALUES
+            ('Diana',        8971.62),
+            ('Josias',       1791.62);
+COMMIT;
 END;
 /
