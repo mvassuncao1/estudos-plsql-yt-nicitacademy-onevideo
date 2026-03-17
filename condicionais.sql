229c6690e5558e37b771a9cadbacab337e660be1
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-17 14:27:32 -0300

    Criados arquivos para condicionais if_else e case

---
+++ condicionais.sql
@@ -0,0 +1,17 @@
+-- YouTube 1:42:38
+
+CONTROL STATEMENT
+
+1. IF statement
+
+    IF CONDITION THEN 
+        STATEMENT;
+    END IF;
+    
+    IF CONDITION THEN
+        STATEMENT;
+    ELSE
+        STATEMENT;
+    END IF;
+
+2. CASE statement
