4a77da85af49a94c2b8bf8e22198042e0d0043ec
Marcos Assunção <mvassuncao1@gmail.com>
2026-03-19 08:22:28 -0300

    Realizado desafio aumento de salário com Cursor

--- cursor_explicito_exercicio.sql
+++ cursor_explicito_exercicio.sql
@@ -1,2 +1,13 @@
---Liste todos os funcionários com salário menor que R$3000 e de um aumento de 15,89%. Retorne ID, nome, salário atual, salário novo
+DECLARE
+v_salario_abaixo_de funcionario.salario%TYPE := 3000; --&valor_minimo_do_salario;
+CURSOR cur_funcionarios(v_salario_abaixo_de funcionario.salario%TYPE) IS
+    SELECT ID_FUNCIONARIO, NOME, SALARIO FROM FUNCIONARIO WHERE SALARIO < v_salario_abaixo_de;
+BEGIN
+FOR funcionario IN cur_funcionarios(v_salario_abaixo_de) LOOP
+    DBMS_OUTPUT.PUT_LINE('Funcionário ' || funcionario.id_funcionario || ' - ' || funcionario.nome || ' / Salário novo: R$ ' || funcionario.salario * 1.1589);
+END LOOP;
+END;
+/
+
+--Liste todos os funcionários com salário menor que R$3000 e dê um aumento de 15,89%. Retorne ID, nome, salário atual, salário novo
 
